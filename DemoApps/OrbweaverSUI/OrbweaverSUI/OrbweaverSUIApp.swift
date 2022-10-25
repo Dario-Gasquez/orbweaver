214f4fa29946edb4bc48001e1cf2009cb84bb5f2
Dario Gasquez <dario.gasquez@gmail.com>
2022-10-25 00:19:35 -0300

    Add Swift/Combine demo app

---
+++ DemoApps/OrbweaverSUI/OrbweaverSUI/OrbweaverSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbweaverSUIApp.swift
+//  OrbweaverSUI
+//
+//  Created by Dario on 24/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct OrbweaverSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
